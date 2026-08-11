db12d4b978d73d74f552c0949479c1b3d627df74
Phoebe Zyla - MSU Student <zylaphoe@tango.private.pa.umd.edu>
2026-08-11 16:22:56 -0400

    seds

--- fermi/plotsed.py
+++ fermi/plotsed.py
@@ -2,7 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-sy_en  = [1.0 5.0 10.0] * 1e6 # TeV -> MeV
+band_edges_MeV = np.array([50,100,300,1000,3000,10000,30000,100000,1000000])
+e_mins = band_edges_MeV[:-1]
+e_maxs = band_edges_MeV[1:]
+e_mids = np.sqrt(e_mins * e_maxs) #geometric mean
+e_err  = e_maxs - e_mids
+
+sy_en  = np.array([1.0,5.0,10.0]) * 1e6 # TeV -> MeV
 sy_erg = sy_en * 1.602e-6     # MeV -> ergs
 
 # Load source CSV #
@@ -11,35 +17,44 @@ df = pd.read_csv("matched_sources.csv",sep=',')
 inputName = df["input_name"]
 fermiName = df["Source_Name"]
 sep_deg   = df["sep_deg"]
+
+flux       = df["Flux_Band"]
+fluxerr_lo = df["Unc_Flux_Band"][:,0]
+fluxerr_hi = df["Unc_Flux_Band"][:,1]
+
+
+nuFnu      = df["nuFnu_Band"]
+with np.errstate(divide='ignore', invalid='ignore'):
+    unc_nufnu_lower = np.where(flux_band > 0,
+        nufnu_band * np.abs(unc_flux_lower) / flux_band,
+        np.nan)
+    unc_nufnu_upper = np.where(flux_band > 0,
+        nufnu_band * np.abs(unc_flux_upper) / flux_band,
+        np.nan)
+
+
 spec_type = df["SpectrumType"]
 pivot     = df["Pivot_Energy"]
+
 ix_PL     = df["PL_Index"]
 flxden_PL = df["PL_Flux_Density"]
+
 ix_LP     = df["LP_Index"]
 flxden_LP = df["LP_Flux_Density"]
 beta_LP   = df["LP_beta"]
-ix_EC     = df["PLEC_Exp_Index"]
-flxden_EC = df["PLEC_Flux_Density"]
 
 
 # begin loop over sources #
 for i, s in enumerate(inputName):
-    sed = pd.read_csv(f"seds/{s}_sed.csv",sep=',').to_numpy()
-    e_mins     = sed[:,1] # MeV
-    e_maxs     = sed[:,2] # MeV
-    e_mids     = sed[:,3]
-    flux       = sed[:,4] # photon cm2 s
-    fluxerr_lo = sed[:,5]
-    fluxerr_hi = sed[:,6]
-    nuFnu      = sed[:,7] # erg cm2 s
-    nuerr_lo   = sed[:,8]
-    nuerr_hi   = sed[:,9]
-
-    e_err  = e_maxs - e_mids   
+    dNdE_PL    = []
+    dNdE_LP    = []
+    nuFnu_PL   = []
+    nuFnu_LP   = []
+    uplim_mask = []
 
     # Extrapolate to my energies #
-    dNdE_PL = flxden_PL * (sy_en/pivot)**ix_PL 
-    dNdE_LP = flxden_LP * (sy_en/pivot)**(-(ix_LP + beta_LP * np.log(sy_en/pivot)))
+    dNdE_PL = flxden_PL[i] * (sy_en/pivot[i])**ix_PL[i] 
+    dNdE_LP = flxden_LP[i] * (sy_en/pivot[i])**(-(ix_LP[i] + beta_LP[i] * np.log(sy_en/pivot[i])))
 
     nuFnu_PL = sy_erg**2 * dNdE_PL/1.602e-6 # erg cm-2 s-1
     nuFnu_LP = sy_erg**2 * dNdE_LP/1.602e-6 
@@ -55,13 +70,31 @@ for i, s in enumerate(inputName):
     plt.errorbar(e_mids,nuFnu,
             yerr=[nuerr_lo,nuerr_hi],
             uplims=uplim_mask,color='r',fmt='none',
-            capsize=10,capthick=1.5,elinewidth=1.5)
-            
+            capsize=10,capthick=1.5,elinewidth=1.5,
+            label="Fermi Data")
+    for x, y in zip(e_mids, nuFnu):
+        plt.annotate(f"{x:.0f}", xy=(x, y), xytext=(0, 8),
+                 textcoords='offset points', 
+                 fontsize=8, ha='center')
+
+    plt.scatter(sy_erg,nuFnu_PL,label="powerlaw",color='green')
+    for x, y in zip(sy_erg, nuFnu_PL):
+        plt.annotate(f"{x:.0f}", xy=(x, y), xytext=(0, 8),
+                 textcoords='offset points',
+                 fontsize=8, ha='center')
+    
+    plt.scatter(sy_erg,nuFnu_LP,label="logPara",color='blue')
+    for x, y in zip(sy_erg, nuFnu_LP):
+        plt.annotate(f"{x:.0f}", xy=(x, y), xytext=(0, 8),
+                 textcoords='offset points',
+                 fontsize=8, ha='center')
+
     plt.yscale('log')
     plt.xscale('log')
     plt.xlabel('Energy [MeV]')
     plt.ylabel(r"$\nu F_{\nu}$ [erg cm$^{-2}$ s$^{-1}$]")
     plt.title(f"{s} SED\n{sep_deg[i]} away from {fermiName[i]}")
+    plt.legend()
     plt.savefig(f"seds/{s}_sed.png")
     plt.close()
     print(f"{s} SED completed")
--- fermi/sourcesfermi.py
+++ fermi/sourcesfermi.py
@@ -6,11 +6,14 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 import csv
 import os
+import matplotlib.pyplot as plt
 
 CATALOG   = "gll_psc_v41.fit"
 SOURCES   = "data14-195.csv"
 SUMMARY   = "matched_sources.csv"
 SED_DIR   = "seds"
+
+summary_rows = []
 
 # Load sources #
 df = pd.read_csv("data14-195.csv",sep='\\s+').to_numpy()
@@ -23,16 +26,6 @@ coords = SkyCoord(
     ra  = RA * u.deg,
     dec = Dec * u.deg,
 )
-
-with fits.open(CATALOG) as hdul:
-    hdul.info()
-    cat = Table(hdul["LAT_Point_Source_Catalog"].data)
-    print(cat.colnames)
-
-cat_coords = SkyCoord(ra = cat["RAJ2000"], dec = cat["DEJ2000"], unit='deg')
-
-# Match my coordinates to the sources in the catalog #
-idx,sep2d,_ = coords.match_to_catalog_sky(cat_coords)
 
 ## Possible column titles ##
 #cols = [
@@ -59,96 +52,150 @@ idx,sep2d,_ = coords.match_to_catalog_sky(cat_coords)
 ## Chosen column titles ##
 available_cols = [
   'Source_Name','RAJ2000','DEJ2000','Signif_Avg', 'Pivot_Energy',
-  'Flux1000', 'Unc_Flux1000', 'Energy_Flux100', 'Unc_Energy_Flux100',
-  'SpectrumType', 'PL_Flux_Density', 'Unc_PL_Flux_Density', 'PL_Index',
-  'Unc_PL_Index', 'LP_Flux_Density', 'Unc_LP_Flux_Density', 'LP_Index',
-  'Unc_LP_Index', 'LP_beta', 'Unc_LP_beta', 'LP_SigCurv', 'LP_EPeak',
-  'Unc_LP_EPeak', 'Sqrt_TS_Band','Signif_Peak', 'Flux_Peak', 'Unc_Flux_Peak',
+  'Flux1000', 'Unc_Flux1000', 'Energy_Flux100', 'Unc_Energy_Flux100', 'SpectrumType', 
+  'PL_Flux_Density', 'Unc_PL_Flux_Density', 'PL_Index', 'Unc_PL_Index',
+  'LP_Flux_Density', 'Unc_LP_Flux_Density', 'LP_Index', 'Unc_LP_Index',
+  'LP_beta', 'Unc_LP_beta', 'LP_SigCurv', 'LP_EPeak', 'Unc_LP_EPeak',
+  'Signif_Peak', 'Flux_Peak', 'Unc_Flux_Peak',
   'ASSOC_FGL','ASSOC1','ASSOC2','RA_Counterpart','DEC_Counterpart',
-  'Unc_Counterpart', 'Flags', 'Flux_Band', 'Unc_Flux_Band','nuFnu_Band'
+  'Unc_Counterpart', 'Flags', 
   ]
 
+array_cols = [
+  'Flux_Band', 'Unc_Flux_Band', 'nuFnu_Band', 'Sqrt_TS_Band'
+  ]
 
-# Energy band edges? #
-band_edges_MeV = np.array([50, 100, 300, 1000, 3000, 10000, 30000, 100000, 1000000])
+## Energy band edges? ##
+band_edges_MeV = np.array([
+    50, 100, 300, 1000, 3000, 
+    10000, 30000, 100000, 1000000
+    ])
+
 e_lo = band_edges_MeV[:-1]
 e_hi = band_edges_MeV[1:]
 e_mids = np.sqrt(e_lo * e_hi)  # geometric mean; log-spaced bins
+sy_en  = np.array([1.0,5.0,10.0]) * 1e6 # TeV -> MeV
+sy_erg = sy_en * 1.602e-6     # MeV -> ergs
 
-# summary rows #
-summary_rows = []
+## Read FITS file ##
+with fits.open(CATALOG) as hdul:
+    cat = Table(hdul["LAT_Point_Source_Catalog"].data)
+    #hdul.info()
+    #print(cat.colnames)
+
+cat_coords = SkyCoord(ra = cat["RAJ2000"], dec = cat["DEJ2000"], unit='deg')
+
+# Match my coordinates to the sources in the catalog #
+idx,sep2d,_ = coords.match_to_catalog_sky(cat_coords)
+
+fermiName = cat[idx]["Source_Name"]
+assoc     = cat[idx]["ASSOC1"]
+fermiRA   = cat[idx]["RAJ2000"]
+fermiDec  = cat[idx]["DEJ2000"]
+
+spectype  = cat[idx]["SpectrumType"]
+pivot     = cat[idx]["Pivot_Energy"]
+
+flxden_PL     = cat[idx]["PL_Flux_Density"]
+flxden_err_PL = cat[idx]["Unc_PL_Flux_Density"]
+ix_PL         = cat[idx]["PL_Index"]
+ix_err_PL     = cat[idx]["Unc_PL_Index"]
+
+flxden_LP     = cat[idx]["LP_Flux_Density"]
+flxden_err_LP = cat[idx]["Unc_LP_Flux_Density"]
+ix_LP         = cat[idx]["LP_Index"]
+ix_err_LP     = cat[idx]["Unc_LP_Index"]
+beta_LP       = cat[idx]["LP_beta"]
+beta_err_LP   = cat[idx]["Unc_LP_beta"]
+
+flux_band      = cat[idx]["Flux_Band"]
+unc_flux_lower = cat[idx]["Unc_Flux_Band"][:,:,0]
+unc_flux_upper = cat[idx]["Unc_Flux_Band"][:,:,1] 
+nufnu          = cat[idx]["nuFnu_Band"]
+
+with np.errstate(divide='ignore', invalid='ignore'):
+    unc_nufnu_lower = np.where(flux_band > 0,
+        nufnu * np.abs(unc_flux_lower) / flux_band,
+        np.nan)
+    unc_nufnu_upper = np.where(flux_band > 0,
+        nufnu * np.abs(unc_flux_upper) / flux_band,
+        np.nan)
+
+## Extrapolate to my energies ##
+# begin loop over sources #
 for i, s in enumerate(sourceName):
+    dNdE_PL    = []
+    dNdE_LP    = []
+    nuFnu_PL   = []
+    nuFnu_LP   = []
+    uplim_mask = []
+
     sep_deg = sep2d[i].deg
-    row = {
-        'input_name': s,
-        'input_ra'  : RA[i],
-        'input_dec' : Dec[i],
-        'sep_deg'   : round(sep_deg, 5),
-    }
-
-    cat_row = cat[idx[i]]
-    for col in available_cols:
-        row[col] = cat_row[col]
-
-    # save source SED #
-    sed_path   = os.path.join(SED_DIR, f"{s}_sed.csv")
-    flux_band  = np.array(cat_row["Flux_Band"])
-    nufnu_band = np.array(cat_row["nuFnu_Band"])
-    flux_unc   = np.array(cat_row["Unc_Flux_Band"])
-
-    unc_flux_lower = cat_row["Unc_Flux_Band"][:, 0]
-    unc_flux_upper = cat_row["Unc_Flux_Band"][:, 1]
-    
-    with np.errstate(divide='ignore', invalid='ignore'):
-        unc_nufnu_lower = np.where(flux_band > 0,
-                                nufnu_band * np.abs(unc_flux_lower) / flux_band,
-                                np.nan)
-        unc_nufnu_upper = np.where(flux_band > 0,
-                                nufnu_band * np.abs(unc_flux_upper) / flux_band,
-                                np.nan)
-
-    with open(sed_path,'w',newline='') as f:
-        writer = csv.writer(f)
-        header = ["band_index","e_min_MeV",'e_max_MeV','e_mid_MeV',
-                      'flux_ph_cm2_s','fluxerr_lo','fluxerr_hi',
-                      'nuFnu_erg_cm2_s','nuFnuerr_lo','nuFnuerr_hi']
-        writer.writerow(header)
-        for b in range(len(flux_band)):
-            writer.writerow([
-                    b,
-                    e_lo[b],
-                    e_hi[b],
-                    e_mids[b],
-                    flux_band[b],
-                    unc_flux_lower[b],
-                    unc_flux_upper[b],
-                    nufnu_band[b],
-                    unc_nufnu_lower[b],
-                    unc_nufnu_upper[b],
-            ])
 
     print(f"Closest source was {sep_deg:.3f} deg away")
-    summary_rows.append(row)
+    summary_rows.append(cat[idx[i]])
+    # Extrapolate to my energies #
+    dNdE_PL = flxden_PL[i] * (sy_en/pivot[i])**ix_PL[i]
+    dNdE_LP = flxden_LP[i] * (sy_en/pivot[i])**(-(ix_LP[i] + beta_LP[i] * np.log(sy_en/pivot[i])))
+
+    nuFnu_PL = sy_erg**2 * dNdE_PL/1.602e-6 # erg cm-2 s-1
+    nuFnu_LP = sy_erg**2 * dNdE_LP/1.602e-6
+
+    # Separate array for upper limits #
+    uplim_mask = np.isnan(unc_flux_lower[i])
+
+    for j in range(len(unc_nufnu_lower[i])):
+        if uplim_mask[j]:
+            unc_nufnu_lower[i][j] = 0.4 * nufnu[i][j]
+
+    plt.figure(layout='constrained')
+
+    plt.errorbar(e_mids,nufnu[i],
+            yerr=[unc_nufnu_lower[i],unc_nufnu_upper[i]],
+            uplims=uplim_mask,color='r',fmt='none',
+            capsize=10,capthick=1.5,elinewidth=1.5,
+            label="Fermi Data")
+    for x, y in zip(e_mids, nufnu[i]):
+        plt.annotate(f"{x:.0f}", xy=(x, y), xytext=(0, 8),
+                 textcoords='offset points',
+                 fontsize=8, ha='center')
+
+    plt.scatter(sy_en,nuFnu_PL,label="powerlaw",color='green')
+    for x, y in zip(sy_en, nuFnu_PL):
+        plt.annotate(f"{x:.0f}", xy=(x, y), xytext=(0, 8),
+                 textcoords='offset points',
+                 fontsize=8, ha='center')
+
+    plt.scatter(sy_en,nuFnu_LP,label="logParabola",color='blue')
+    for x, y in zip(sy_en, nuFnu_LP):
+        plt.annotate(f"{x:.0f}", xy=(x, y), xytext=(0, 8),
+                 textcoords='offset points',
+                 fontsize=8, ha='center')
+
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.xlabel('Energy [MeV]')
+    plt.ylabel(r"$\nu F_{\nu}$ [erg cm$^{-2}$ s$^{-1}$]")
+    plt.title(f"{s} SED\n{sep_deg} away from {fermiName[i]}")
+    plt.legend()
+    plt.savefig(f"seds/{s}_sed.png")
+    plt.close()
+    print(f"{s} SED completed")
+
+
+## Summary rows ##
 
 ## Write csv ##
 priority_cols = ["input_name", "input_ra", "input_dec", "sep_deg",
                  "Source_Name", "ASSOC1", "RAJ2000", "DEJ2000",
-                 "SpectrumType",
+                 "SpectrumType", "Signif_Avg", "Pivot_Energy"
                  ]
 
-all_fields = set()
-for r in summary_rows:
-    all_fields.update(r.keys())
-
-remaining = sorted(all_fields - set(priority_cols))
-fieldnames = priority_cols + remaining
-
 with open(SUMMARY,'w',newline='') as f:
-    writer = csv.DictWriter(f,fieldnames=fieldnames)
+    writer = csv.DictWriter(f,fieldnames=priority_cols)
     writer.writeheader()
     writer.writerows(summary_rows)
 
 print(f"\nDone. Summary written to {SUMMARY}")
-print(f"Per-source SEDs (those available, at least) written to {SED_DIR}/")
 
 
