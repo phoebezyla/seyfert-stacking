3863ea61fdacc591be6c0d553f4fd96834c56c72
Phoebe Zyla - MSU Student <phoebe.zyla@gmail.com>
2026-09-22 17:00:18 -0400

    color

--- crabtests/plot_spec.py
+++ crabtests/plot_spec.py
@@ -1,30 +1,42 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.optimize import curve_fit
+
+def PowerLaw(x,A,pivot,gamma):
+    return A * (x/pivot) ** (-gamma)
+
 
 IX = ['2','27','3']
 inds = [2.0,2.7,3.0]
-NUM = ['one','two']
-c = plt.cm.tab10(np.linspace(0, 1, len(NUM)*len(IX))).reshape(len(NUM), len(IX), 4) 
+#NUM = ['one','two']
+num = 'one'
+PIV = [1e9,5e9,10e9]
 
-xarr = np.linspace(0.5,15,10000)  # in TeV
+xxs = np.logspace(np.log10(0.5e9),np.log10(100e9),200)
+xarr = np.logspace(np.log10(0.5e9),np.log10(100e9),1000)
 
-
+c = plt.cm.tab10(np.linspace(0, 1, len(IX))).reshape(len(IX), 4) 
 plt.figure(figsize=[10,8],layout='constrained')
 
 for j,ix in enumerate(IX):
-    for i,num in enumerate(NUM):
-        df = pd.read_csv(f"mod-nopiv-stacked-ind{ix}-{num}crab-results.csv")
-        pivot = df['pivot']
-        indminNorm = df['indminNorm']
+    norms = []
+    df = pd.read_csv(f"mod-nopiv-stacked-ind{ix}-{num}crab-results.csv")
+    pivot = df['pivot']
+    indminNorm = df['indminNorm']
+    
+    norms_cols = [f"norms_{k}" for k in range(200)]
+    norms_mat = df[norms_cols].values   # (3 rows, 200 vals)    
 
-        plt.scatter(pivot,indminNorm,color=c[i,j],label=f"Index = {ix}, {num} crabs")
-   
-        for k,pv in enumerate(pivot):
-            y = indminNorm[k] * (xarr/pv) ** -(inds[j]) 
-            plt.plot(xarr,y,color=c[i,j],label='_nolegend_')
+    for row in range(norms_mat.shape[0]):
+        norms = norms_mat[row,:]
+        popt, pcov = curve_fit(PowerLaw,xxs,norms,maxfev=10000)
+    
+        plt.plot(xxs,norms,color=c[j],label=f"Index = {ix}")
+        plt.plot(xarr,PowerLaw(xarr,*popt),color=c[j],
+            label=f"Fit Powerlaw; A = {popt[0]}, piv = {popt[1]}, gam = {popt[2]}")
 
-
+plt.xscale('log')
 plt.yscale('log')
 plt.xlabel("Pivot energy [TeV]")
 plt.ylabel("Normalization")
